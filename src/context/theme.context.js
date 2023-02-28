20323f74723526f95b8c74d13b03c6cc730ca9a5
Julian Rivera <julianarevircoins@gmail.com>
2023-02-28 12:13:41 -0500

    theme fix

--- src/context/theme.context.js
+++ src/context/theme.context.js
@@ -5,12 +5,13 @@ import { useState } from "react";
 const ThemeContext = createContext();
 
 function ThemeProviderWrapper({ children }) {
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(true);
   const [mode, setMode] = useState("light");
   //   const mode = "dark";
   //   let lightMode = "light";
   const toggleTheme = () => {
     setToggle(!toggle);
+    console.log(toggle);
     setMode(toggle ? "dark" : "light");
   };
   return (
